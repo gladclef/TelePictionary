5880e3aa8740d2abdc2cf7925c7e8d301d6af287
Benjamin Bean <gladclef@users.noreply.github.com>
2020-05-18 12:24:15 -0600

    modified toExec.js to allow for other startup-time code to execute

--- js/toExec.js
+++ js/toExec.js
@@ -3,35 +3,49 @@ if (window.a_toExec === undefined) window.a_toExec = [];
 var execWaitingFuncs = null;
 execWaitingFuncs = function()
 {
-	var b_funcStarted = true;
-	while (b_funcStarted)
+	var b_allDependenciesFound = false;
+
+	// Iterate through the waiting functions in a_toExec until one is found that has all of its dependencies met.
+	// Once one such function is found, register it, execute it, and delay to allow other javascript to execute.
+	for (var i = 0; i < a_toExec.length; i++)
 	{
-		b_funcStarted = false;
-		for (var i = 0; i < a_toExec.length; i++)
+		var o_funcObj      = a_toExec[i];
+		var s_name         = o_funcObj["name"];
+		var a_dependencies = o_funcObj["dependencies"];
+		var f_func         = o_funcObj["function"];
+
+		b_allDependenciesFound = true;
+		for (var j = 0; j < a_dependencies.length; j++)
 		{
-			var o_funcObj = a_toExec[i];
-			s_name = o_funcObj["name"];
-			a_dependencies = o_funcObj["dependencies"];
-			f_func = o_funcObj["function"];
-
-			var b_allDependenciesFound = true;
-			for (var j = 0; j < a_dependencies.length; j++)
-			{
-				var s_dependency = a_dependencies[j];
-				if (window[s_dependency] === undefined) {
-					b_allDependenciesFound = false;
-				}
-			};
-
-			if (b_allDependenciesFound)
-			{
-				window[s_name] = true;
-				f_func();
-				b_funcStarted = true;
-				a_toExec.splice(i,1);
+			var s_dependency = a_dependencies[j];
+			if (window[s_dependency] === undefined) {
+				b_allDependenciesFound = false;
+				break;
 			}
+		};
+
+		if (b_allDependenciesFound)
+		{
+			window[s_name] = true;
+			f_func();
+
+			// remove this function from a_toExec so that it doesn't execute again
+			a_toExec.splice(i,1);
+
+			// allow other javascript to execute
+			break;
 		}
 	}
-	setTimeout(execWaitingFuncs, 100);
+
+	if (b_allDependenciesFound) {
+		// Run again, but from the back of the javascript queue.
+		// Allows for other queued javascript (eg in <script> tags) to execute.
+		setTimeout(execWaitingFuncs, 0);
+	} else {
+		// Run again, but in 100ms.
+		// Allows time for currently unqueued a_toExec functions to be enqueued.
+		// Also prevents this script from eating up all the browser's processing.
+		setTimeout(execWaitingFuncs, 100);
+	}
 };
 window.addEventListener('load', execWaitingFuncs);
